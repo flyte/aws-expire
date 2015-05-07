68cc02300c172b2117b1e28a711141863e2ec8ba
Flyte <flyte@failcode.co.uk>
2015-05-07 09:27:13 +0100

    Add some test code

---
+++ tests/awsexpire/mock/boto/ec2.py
@@ -0,0 +1,5 @@
+# TODO: Import log_args from our util module
+
+@log_args
+def connect_to_region(region, access_key_id, secret_access_key):
+    pass
---
+++ tests/awsexpire/mock/util.py
@@ -0,0 +1,14 @@
+import logging
+log = logging.getLogger("mockboto")
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.DEBUG)
+
+def log_args(func):
+    def log_args_wrapper(*args, **kwargs):
+        arg_strs = []
+        [arg_strs.append(x) for x in args]
+        [arg_strs.append("%s=%s" % (k, v)) for k, v in kwargs.items()]
+        msg = "%s(%s)" % (func.func_name, ", ".join(arg_strs))
+        log.info("MOCK FUNCTION: %s" % msg)
+        func(*args, **kwargs)
+    return log_args_wrapper
